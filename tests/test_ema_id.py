7ea629fd6a6c8d1a8523516a2d40f01510a09cf9
Kajetan <kajetan.dt@portacapena.com>
2023-12-29 20:16:28 +0100

    tests for init ema_it with fields full_id

--- tests/test_ema_id.py
+++ tests/test_ema_id.py
@@ -5,13 +5,16 @@ from src.ema_id_tool import ema_id
 class TestEmaID(unittest.TestCase):
 
     def setUp(self):
-        self.fields_init_id = ema_id.EmaID(country_code='NL', provider_id='XYZ', id_type='C', ema_instance='3KD88D0N')
+        pass
 
-    def test_get_check_digit(self):
-        self.assertEqual(self.fields_init_id.get_check_digit(), 'Y')
+    def test_fields_init(self):
+        eid = ema_id.EmaID(country_code='NL', provider_id='XYZ', id_type='C', ema_instance='3KD88D0N')
+        self.assertEqual(eid.get_check_digit(), 'Y')
+        self.assertEqual(eid.get_full_id(), 'NLXYZC3KD88D0NY')
+        self.assertEqual(eid.get_full_id_with_dashes(), 'NL-XYZ-C3KD88D0N-Y')
 
-    def test_get_full_id(self):
-        self.assertEqual(self.fields_init_id.get_full_id(), 'NLXYZC3KD88D0NY')
-
-    def test_get_full_id_with_dashes(self):
-        self.assertEqual(self.fields_init_id.get_full_id_with_dashes(), 'NL-XYZ-C3KD88D0N-Y')
+    def test_full_id_init(self):
+        eid = ema_id.EmaID(ema_id='NLXYZC3KD88D0N')
+        self.assertEqual(eid.get_check_digit(), 'Y')
+        self.assertEqual(eid.get_full_id(), 'NLXYZC3KD88D0NY')
+        self.assertEqual(eid.get_full_id_with_dashes(), 'NL-XYZ-C3KD88D0N-Y')
